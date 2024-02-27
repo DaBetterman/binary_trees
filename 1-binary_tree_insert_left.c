4062d5455dd3849a284e924bc368b6c3621260e8
DaBetterman <1ebrahimr@gmail.com>
2024-02-27 20:58:48 +0200

    update_task_1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 	{
 		perror("malloc");
-		return (0);
+		return (NULL);
 	}
 	new_node->n = value;
 	new_node->left = NULL;
