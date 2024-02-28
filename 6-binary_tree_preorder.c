8a9826de64f8687178356c49059faf08374a6835
DaBetterman <1ebrahimr@gmail.com>
2024-02-28 16:20:19 +0200

    update task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,10 +1,9 @@
 /**
- * 
- * 
- * 
- * 
- * 
-*/
+ * binary_tree_preorder - Goes through a binary tree using pre-order traversal
+ *
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+ */
 
 #include "binary_trees.h"
 
@@ -19,5 +18,4 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
-
 }
