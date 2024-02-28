c49909456b833dd358b2c32b9024eee80ce1abee
DaBetterman <1ebrahimr@gmail.com>
2024-02-28 13:58:05 +0200

    updte task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (new_node == NULL)
 	{
-		/* perror("malloc"); */
+		fprintf(stderr, "Malloc error\n");
 		return (NULL);
 	}
 
